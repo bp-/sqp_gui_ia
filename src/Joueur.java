59dd7c516d8ea9e517ed3bc283fe992c4e0641ba
rubensadoun <rubensadoun@hotmail.fr>
2013-10-09 15:52:17 +0200

    méthode qui actualise la main du joueur

--- src/Joueur.java
+++ src/Joueur.java
@@ -1,18 +1,43 @@
 import moteur.*;
 import java.math.*;
-
+import moteur.SauveQuiPuce;
 
 
 
 public class Joueur implements moteur.Joueur {
 	
 	private Carte[] mainDuJoueur;
+	public int numeroJoueur;
+	private SauveQuiPuce moteur;
+	
+	public Joueur(SauveQuiPuce pmoteur){
+		
+		this.moteur=pmoteur;
+	}
+	
 	
 	public Coup prochainCoup(Coup[] coupsPossibles){
 		
 		
 		return coupsPossibles[(int)(Math.random() * (coupsPossibles.length+1)) ];
 	}
-	
+   
+	private Carte[] nouvelleMain(Carte[] mainDuJoeur){
+		
+		
+		Carte[] carte2= new Carte[mainDuJoueur.length+1];
+	    Coup coup=moteur.getDernierCoup(numeroJoueur);
+		
+		if (moteur.getDernierCoup(numeroJoueur) instanceof Prendre){
+			
+			Prendre temp= (Prendre) coup;
+			
+			
+			
+			carte2[carte2.length+1]=temp.getCarte();
+			
+		}
+		return carte2;
+	}
 
 }
