40d73b74d1bfba65d336690232d1b9ecf6f360b5
rubensadoun <rubensadoun@hotmail.fr>
2013-10-15 09:06:16 +0200

    test commit

--- src/Joueur.java
+++ src/Joueur.java
@@ -7,7 +7,6 @@ public class Joueur implements moteur.Joueur {
 	public static int nbreJoueurs = 0;
 	
 	private Carte[] mainDuJoueur;
-<<<<<<< HEAD
 	public int numeroJoueur;
 	private SauveQuiPuce moteur;
 	
@@ -16,17 +15,22 @@ public class Joueur implements moteur.Joueur {
 		this.moteur=pmoteur;
 	}
 	
-=======
+
 	private int numDuJoueur;
 	
 	public Joueur() {
 		nbreJoueurs++;
 	}
->>>>>>> ed1175f456397befbdaabc4788479c333c771863
+
 	
 	public Coup prochainCoup(Coup[] coupsPossibles){
-		System.out.println("w00t");
+
+		
+
+		
+		
 		return coupsPossibles[(int)(Math.random() * (coupsPossibles.length+1)) ];
+
 	}
    
 	private Carte[] nouvelleMain(Carte[] mainDuJoeur){
